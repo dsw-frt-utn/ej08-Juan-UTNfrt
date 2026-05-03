dfde7025902e533925476aef17c5caca23729110
Juan-UTNfrt <Juan.Cejas@alu.frt.utn.edu.ar>
2026-05-03 18:36:04 -0300

    Resulucion del Problema 5 del tp8

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        public decimal amount;
+
+        public decimal GetAmount()
+        {
+            return amount;
+        }
+
+        public void SetAmount(decimal amount)
+        {
+            this.amount = amount;
+        }
+
+        public virtual decimal CalculateTotal()
+        {
+            return amount;
+        }
+    }
+
+    public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return GetAmount();
+        }
+    }
+
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return GetAmount() * 0.90m;
+        }
+    }
+
+    public class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+}
